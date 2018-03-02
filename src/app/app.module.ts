6857c51f5836136fb660c09eb27abad341a971e4
woltenX <wolten@gmail.com>
2018-03-02 13:03:09 -0700

    Comenzando a poner el tema ADMINPRO

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,27 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { GraficasComponent } from './pages/graficas/graficas.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { BreadcumbComponent } from './shared/breadcumb/breadcumb.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NopagefoundComponent,
+    DashboardComponent,
+    ProgressComponent,
+    GraficasComponent,
+    HeaderComponent,
+    SidebarComponent,
+    BreadcumbComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/shared/breadcumb/breadcumb.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-breadcumb',
+  templateUrl: './breadcumb.component.html',
+  styles: []
+})
+export class BreadcumbComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
